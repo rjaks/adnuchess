a87544a84bb42733bf9ac69caf5e1713485b9f2d
Adrian <107988407+rjaks@users.noreply.github.com>
2025-09-20 21:53:58 +0800

    New: Added dependencies in package.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', 'convex-nuxt'],
   tailwindcss: { cssPath: '~/assets/css/tailwind.css' },
   css: [],
-  compatibilityDate: '2025-09-19'
+  compatibilityDate: '2025-09-19',
+  convex: {
+    url: process.env.CONVEX_URL
+  }
 })
